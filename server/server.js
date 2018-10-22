55e25711ae3142da55fc7b5acd44b8bbc5ae65ff
joshua-hurn <joshua.hurn@hotmail.com>
2018-10-22 16:27:13 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const apiRouter = require('./routes');
+
+let app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use('/api', apiRouter);
+
+let port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
